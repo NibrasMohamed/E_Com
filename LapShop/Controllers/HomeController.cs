eef85d85e08d9df473cfbd1c9217d0d658799f66
NibrasMohamed <nibras1998529@gmail.com>
2023-03-26 13:57:08 +0530

    Cart and Product

--- LapShop/Controllers/HomeController.cs
+++ LapShop/Controllers/HomeController.cs
@@ -28,5 +28,15 @@ namespace LapShop.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Products()
+        {
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
     }
 }
